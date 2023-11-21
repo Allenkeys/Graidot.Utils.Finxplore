1cd860f4f174eec6316cb2e29f603a62bc59546c
Joshua <79511167+Allenkeys@users.noreply.github.com>
2023-11-21 15:02:06 +0100

    Setup institute types and extension method for their names

---
+++ Graidot.Utils.Finxplore/Enums/InstituteType.cs
@@ -0,0 +1,10 @@
+﻿namespace Graidot.Utils.Finxplore.Enums
+{
+    public enum InstituteType
+    {
+        CommercialBank = 1,
+        MicroFinanceBank,
+        TrustFund,
+        MerchantBank
+    }
+}
---
+++ Graidot.Utils.Finxplore/Extensions/InstituteTypeExtension.cs
@@ -0,0 +1,22 @@
+﻿using Graidot.Utils.Finxplore.Enums;
+
+namespace Graidot.Utils.Finxplore.Extensions
+{
+    public static class InstituteTypeExtension
+    {
+        public static string ToStringValue(this InstituteType type)
+        {
+            switch (type)
+            {
+                case InstituteType.CommercialBank:
+                    return "Commercial-Bank";
+                case InstituteType.MicroFinanceBank:
+                    return "Microfinance-Bank";
+                case InstituteType.TrustFund:
+                    return "Trust-Fund";
+                default:
+                    return "";
+            }
+        }
+    }
+}
