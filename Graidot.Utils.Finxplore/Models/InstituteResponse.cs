fdab8fbd0c3acbf6b3e3d733e7f6c3419493cecc
Joshua <79511167+Allenkeys@users.noreply.github.com>
2023-11-22 08:39:03 +0100

    Added institute response dto

---
+++ Graidot.Utils.Finxplore/Models/InstituteResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graidot.Utils.Finxplore.Models
+{
+    public sealed class InstituteResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Institute { get; set; }
+    }
+}
