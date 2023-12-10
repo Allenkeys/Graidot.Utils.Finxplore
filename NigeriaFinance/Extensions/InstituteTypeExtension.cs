96694b4110ed6c2bf280af4074bba54f9da5081b
Joshua <79511167+Allenkeys@users.noreply.github.com>
2023-12-10 10:58:02 +0100

    Update enum functions

--- NigeriaFinance/Extensions/InstituteTypeExtension.cs
+++ NigeriaFinance/Extensions/InstituteTypeExtension.cs
@@ -8,10 +8,8 @@ namespace NigeriaFinance.Extensions
         {
             switch (type)
             {
-                case InstituteType.CommercialBank:
+                case InstituteType.Bank:
                     return "Banks.min.json";
-                case InstituteType.MicroFinanceBank:
-                    return "Microfinance.min.json";
                 case InstituteType.PensionFund:
                     return "Pensionfunds.min.json";
                 default:
