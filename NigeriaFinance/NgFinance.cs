38cbb1d36f244c117653c414789c4dc8196755b0
Joshua <79511167+Allenkeys@users.noreply.github.com>
2023-12-11 20:02:33 +0100

    Added xml documentatiuons core open methods

--- NigeriaFinance/NgFinance.cs
+++ NigeriaFinance/NgFinance.cs
@@ -13,11 +13,25 @@ namespace NigeriaFinance
         {
             _service = new FinanceInstituteServices();
         }
+
+        /// <summary>
+        /// Gets a financial institution. Note: The financial institutions depend on the type passed as parameter 
+        /// </summary>
+        /// 
+        /// <param name="typeId"><see cref="InstituteType"/>The type of financial institution from NigeriaFinance.Enums.InstituteType</param>
+        /// <param name="instituteId"><see cref="int"/> The institution ID</param>
+        /// <returns> <see cref="InstituteResponse"/> a financial institution</returns>
         public static async Task<InstituteResponse> GetInstituteAsync(InstituteType typeId, int instituteId)
         {
             return await _service.GetInstituteAsync(typeId, instituteId);
         }
 
+        /// <summary>
+        /// Gets a collection of financial institutions. 
+        /// Note: These financial institutions depend on the type passed as parameter 
+        /// </summary>
+        /// <param name="typeId"><see cref="InstituteType"/> The type of financial institution.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="InstituteResponse"/> of financial institutions</returns>
         public static async Task<IEnumerable<InstituteResponse>> GetInstitutesAsync(InstituteType typeId)
         {
             return await _service.GetInstitutesAsync(typeId);
